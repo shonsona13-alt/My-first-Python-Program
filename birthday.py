8a1963d2463dad777010c1622c620806d2f32fcf
Shonsona13 <shonsona13@gmail.com>
2026-09-04 17:54:05 -0500

    Add files via upload

---
+++ birthday.py
@@ -0,0 +1,7 @@
+friend1 = "Sam"
+friend2 = "Alex"
+friend3 = "Jordan"
+friend4 = "Taylor"
+friend5 = "Noah"
+
+print("Welcome to Raj's birthday party! Here are the friends who are invited:", friend1, ",", friend2, ",", friend3, ",", friend4, ", and", friend5 + ".")
